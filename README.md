7bcd783646efb2c8c21b828ad59a4d7493c0c9fd
yoshi <hogehoge@com>
2019-02-20 11:25:27 +0900

    firstcommit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Preperation
+Create mysite/mysite/.env
+```
+SOCIAL_AUTH_GITHUB_KEY = ''
+SOCIAL_AUTH_GITHUB_SECRET = ''
+SOCIAL_AUTH_TWITTER_KEY = ''
+SOCIAL_AUTH_TWITTER_SECRET = ''
+SOCIAL_AUTH_FACEBOOK_KEY = ''  # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = ''  # App Secret
+```
+
+# Execution
+```
+% cd mysite/
+% docker-compose build
+% docker-compose up
+```
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,33 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+
+from mysite.login import views as login_views
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='base.html') , name='base'),
+    url(r'^home/$', login_views.home, name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^chat/', include('chat.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
